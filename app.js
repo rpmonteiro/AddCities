25cf927220d1435545148fc43d2da0a5118e2ecd
Ricardo <ric.p.monteiro@gmail.com>
2015-10-27 20:29:25 +0100

    Delete cities

--- app.js
+++ app.js
@@ -35,4 +35,11 @@ app.post('/cities', urlencode, function(req, res) {
 	});
 });
 
+app.delete('/cities/:name', function(req, res) {
+	client.hdel('cities', req.params.name, function(error) {
+		if (error) throw error;
+		res.sendStatus(204);
+	})
+})
+
 module.exports = app;
