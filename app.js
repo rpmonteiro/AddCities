6bd1e16b3fd9e72c433e76334e99d3144614c3d8
Ricardo <ric.p.monteiro@gmail.com>
2015-10-27 21:57:10 +0100

    Add show and extract routes

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var urlencode = bodyParser.urlencoded({ extended: false});
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static('public'));
-
-// Redis connection
-var redis = require('redis');
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
-} else {
-  var client = redis.createClient();
-  client.select((process.env.NODE_ENV || 'development').length);
-}
-// End Redis Connection
-
-app.get('/cities', function(req, res) {
-	client.hkeys('cities', function(error, names) {
-		if(error) throw error;
-
-		res.json(names);
-	})
-});
-
-app.post('/cities', urlencode, function(req, res) {
-	var newCity = req.body;
-	client.hset('cities', newCity.name, newCity.description, function(error) {
-		if(error) throw error;
-		
-		res.status(201).json(newCity.name);
-	});
-
-	if (!newCity.name || !newCity.description) {
-		res.sendStatus(400);
-		return false;
-	};
-});
-
-app.delete('/cities/:name', function(req, res) {
-	client.hdel('cities', req.params.name, function(error) {
-		if (error) throw error;
-		res.sendStatus(204);
-	})
-})
+var cities = require('./routes/cities');
+app.use('/cities', cities);
 
 module.exports = app;
