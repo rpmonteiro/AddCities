498e606fab3674c8a1174343913203c70ae43e74
Ricardo <ric.p.monteiro@gmail.com>
2015-10-27 19:51:04 +0100

    replace in memory with redis

--- app.js
+++ app.js
@@ -6,20 +6,34 @@ var urlencode = bodyParser.urlencoded({ extended: false});
 
 app.use(express.static('public'));
 
-var cities = {
-	'Lotopia': 'This city sucks',
-	'Caspiana': 'Better than yours!',
-	'Indigo': 'Uhhh..scary...'
-};
+//Redis Connection
+var redis = require('redis');
+if (process.env.REDISTOGO_URL) {
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var client = require("redis").createClient(rtg.port, rtg.hostname);	
+	client.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = redis.createClient();
+}
+client.select((process.env.NODE_ENV || 'development').length);
+var client = redis.createClient();
+//End Redis Connection
 
 app.get('/cities', function(req, res) {
-	res.json(Object.keys(cities));	
+	client.hkeys('cities', function(error, names) {
+		if(error) throw error;
+
+		res.json(names);
+	})
 });
 
 app.post('/cities', urlencode, function(req, res) {
 	var newCity = req.body;
-	cities[newCity.name] = newCity.description;
-	res.status(201).json(newCity.name);
-})
+	client.hset('cities', newCity.name, newCity.description, function(error) {
+		if(error) throw error;
+		
+		res.status(201).json(newCity.name);
+	});
+});
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
 
 describe('Requests to the root path', function() {
 	it('Returns a 200 status code', function(done) {
@@ -38,8 +42,7 @@ describe('Listing cities on /cities', function () {
 	it('Returns initial cities', function (done) {
 		request(app)
 			.get('/cities')
-			.expect(JSON.stringify(['Lotopia',
-				'Caspiana', 'Indigo']), done);
+			.expect(JSON.stringify([]), done);
 	});
 });
 
