b81c23f645daa65bdfc2fe190f698aaa16ccaa6a
Ricardo <ric.p.monteiro@gmail.com>
2015-10-27 20:05:38 +0100

    No select for production FIXED

--- app.js
+++ app.js
@@ -6,18 +6,17 @@ var urlencode = bodyParser.urlencoded({ extended: false});
 
 app.use(express.static('public'));
 
-//Redis Connection
+// Redis connection
 var redis = require('redis');
 if (process.env.REDISTOGO_URL) {
-	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var client = require("redis").createClient(rtg.port, rtg.hostname);	
-	client.auth(rtg.auth.split(":")[1]);
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
 } else {
-    var redis = redis.createClient();
-    client.select((process.env.NODE_ENV || 'development').length);
+  var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
 }
-var client = redis.createClient();
-//End Redis Connection
+// End Redis Connection
 
 app.get('/cities', function(req, res) {
 	client.hkeys('cities', function(error, names) {
